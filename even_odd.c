c315c73b18824be02ca6399c3afd80989c9e4a5b
shruti-pd20 <devkarshruti882@gmail.com>
2026-06-17 23:11:21 +0530

    Added even_odd.c program

---
+++ even_odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("enter a number:");
+    scanf("%d",&num);
+    if(num % 2 == 0)
+    printf("even number");
+    else
+    printf("odd number");
+    return 0;
+}
