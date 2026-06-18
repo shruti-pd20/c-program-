a072a7f884f4e04dfa543db09f8a6ea5b2c22c77
shruti-pd20 <devkarshruti882@gmail.com>
2026-06-18 14:38:42 +0530

    Added division.c program

---
+++ division.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int a,b,div;
+    printf("enter any two numbers:");
+    scanf("%d %d", &a, &b);
+    div=a/b;
+    printf("division=%d",div);
+    return 0;
+}
