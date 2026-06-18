6875d196f9eb026bea6d0976e70c2824508222f5
shruti-pd20 <devkarshruti882@gmail.com>
2026-06-18 14:05:52 +0530

    Added substraction.c program

---
+++ substraction.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1,num2,sub;
+    printf("enter any two numbers:");
+    scanf("%d %d", &num1, &num2);
+    sub = num1-num2;
+    printf("substraction=%d",sub);
+    return 0;
+}
